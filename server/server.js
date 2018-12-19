07e92fa6727c46d70411aae2d3589cdfccbddce8
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2018-12-19 17:47:18 +0500

    connecting to mlab

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql")
 const schema = require("./Schema/schema")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://<dbuser>:<password>@ds161653.mlab.com:61653/graphql-practice")
+
+mongoose.connection.once("open", () => {
+    console.log("connected to database")
+})
 
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql : true
+    graphiql: true
 }))
 
-app.listen(3000, ()=>{
-    console.log("your server is running on 3000" )
+app.listen(3000, () => {
+    console.log("your server is running on 3000")
 })
